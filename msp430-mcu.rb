c21b9e9ed4ee6c9ee7d6679d5a4874597b7d388c
Beshr Al Nahas <beshr@sics.se>
2015-01-11 11:00:48 +0100

    Update version to 20120716

--- msp430-mcu.rb
+++ msp430-mcu.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class Msp430Mcu < Formula
   homepage 'http://mspgcc.sourceforge.net'
-  url 'http://sourceforge.net/projects/mspgcc/files/msp430mcu/msp430mcu-20120406.tar.bz2'
-  sha1 'c096eec84f0f287c45db713a550ec50c518fa065'
-
+  # url 'http://sourceforge.net/projects/mspgcc/files/msp430mcu/msp430mcu-20120406.tar.bz2'
+  # sha1 'c096eec84f0f287c45db713a550ec50c518fa065'
+  url 'http://sourceforge.net/projects/mspgcc/files/msp430mcu/msp430mcu-20120716.tar.bz2'
+  
   def install
     # Create the "bin" directory for installation. The install.sh script
     # expects this to exist.
